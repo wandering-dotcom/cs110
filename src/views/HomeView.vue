aa316dfb64318c9d9c5702460e86b6c96cbe6b31
wandering-dotcom <dvan037@ucr.edu>
2025-07-08 15:26:51 +0900

    login & password on login now, still need to change around names

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="login">
+    <h1>This is an about page</h1>
+  </div>
 </template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
